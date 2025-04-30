28f576bc643a6002b62914bbed3bfa77ee8639f2
Yin Cheang Ng <ngyc04169@gmail.com>
2025-04-30 13:01:59 +0800

    update

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ rm -rf build
 cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/opt/homebrew/lib/cmake
 cmake --build build
 
-# ./build/main
-# ./build/alphawrap data/initial_struct.obj 600 600
+./build/main
+./build/alphawrap data/initial_struct.obj 600 600
 ./build/levelset
 
 
