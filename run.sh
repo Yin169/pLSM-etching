3fcf8981f644dd17fce6fc58f2fffe2592f4798d
Yin Cheang Ng <ngyc04169@gmail.com>
2025-05-01 15:24:50 +0800

    update

--- run.sh
+++ run.sh
@@ -8,11 +8,7 @@ cmake --build build
 # Run the OpenVDB-based level set method for etching simulation
 # ./build/vdbLevelset initial_struct_600_600.obj etched_result.obj 1.0 100
 
-# Uncomment to run the original level set method
-./build/levelset
-
-# Uncomment to run other executables
 # ./build/main
 # ./build/alphawrap data/initial_struct.obj 600 600
-
+./build/levelset
 
