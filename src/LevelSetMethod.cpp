7d2ea1d33a4ca3c7705ef811fdd0c1d99e8165a2
Yin Cheang Ng <ngyc04169@gmail.com>
2025-05-02 13:01:46 +0800

    Update LevelSetMethod.cpp

--- src/LevelSetMethod.cpp
+++ src/LevelSetMethod.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
     
     testOpenMP();
        
-    LevelSetMethod levelSet(inputFile, 400, 0.01, 10000, 10);
+    LevelSetMethod levelSet(inputFile, 400, 0.01, 1000, 10);
 
     // Run the level set evolution
     std::cout << "Running level set evolution..." << std::endl;
