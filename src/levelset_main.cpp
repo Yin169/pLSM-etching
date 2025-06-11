56ce1545cbd6ad0b3b99db5a0b98811ed0fdff9c
Yin Cheang Ng <ngyc04169@gmail.com>
2025-06-11 14:43:43 +0800

    add it back

---
+++ src/levelset_main.cpp
@@ -0,0 +1,150 @@
+#include <omp.h>
+#include <iostream>
+#include <cmath>
+#include <filesystem>
+#include <eigen3/Eigen/Core>
+
+namespace fs = std::filesystem;
+
+#include "convert.hpp"
+#include "alphawrap.hpp"
+#include "OBJToBNDConverter.hpp"
+
+#include "LevelSetMethod.hpp"
+#include "LevelSetMethod.cpp"
+
+int main(int argc, char* argv[]) {
+
+    Eigen::initParallel();
+    std::string folderPath = "./data";
+
+    if (!fs::exists(folderPath)) {
+        if (fs::create_directory(folderPath)) {
+            std::cout << "Directory created successfully.\n";
+        } else {
+            std::cerr << "Failed to create directory.\n";
+        }
+    } else {
+        std::cout << "Directory already exists.\n";
+    }
+
+    const std::string inFile = argv[1];
+    const std::string outputFile = argv[2];
+    std::cout << "Input file: " << inFile << std::endl;
+    std::cout << "Output file: " << outputFile << std::endl;
+
+    Convert(inFile);
+    Wrapper("data/initial_struct.obj", 600, 600);
+
+    std::string inputFile = "initial_struct_600_600.obj";
+    
+    LevelSetMethod levelSet(
+        inputFile,
+        200,    // gridSize
+        0.1,   // timeStep
+        60,    // maxSteps
+        3,      // reinitInterval
+        TimeSchemeType::RUNGE_KUTTA_3,
+        -1     // numThreads (auto)
+    );
+
+    levelSet.setGridMaterial("SiO2_PECVD", 500, 400); 
+    levelSet.setGridMaterial("Si_Amorph", 400, 364); 
+    levelSet.setGridMaterial("Polymer", 364, 304);
+    levelSet.setGridMaterial("Si3N4_LPCVD", 304, 204); 
+    levelSet.setGridMaterial("SiO2_Thermal", 204, 203); 
+    levelSet.setGridMaterial("Si_Xtal", 203, -200); 
+    // levelSet.exportGridMaterialsToCSV("checking.csv");
+    levelSet.updateU();
+
+
+    // case 1
+    levelSet.clearMaterialProperties();
+    levelSet.setMaterialProperties("Polymer", 0.1, 0.01);
+    levelSet.setMaterialProperties( "SiO2_PECVD", 0.6, 0.01);
+    levelSet.setMaterialProperties("Si_Amorph", 1.0, 0.01);
+    std::string surfaceFile = "Silicon_etch.obj";
+    std::string outputBNDfile = "Silicon_etch.bnd";
+    surfaceFile = outputFile + surfaceFile;
+    outputBNDfile = outputFile + outputBNDfile;
+
+    std::cout << "Running level set evolution..." << std::endl;
+    if (!levelSet.evolve()) {
+        std::cerr << "Evolution failed. Exiting." << std::endl; return 1;
+    }
+    std::cout << "Saving surface mesh to " << surfaceFile << "..." << std::endl;
+    if (!levelSet.extractSurfaceMeshCGAL(surfaceFile)) {
+        std::cerr << "Failed to save surface mesh. Exiting." << std::endl;
+        return 1;
+    }
+
+    int saveBND = ConvertOBJToDFISE(surfaceFile, outputBNDfile); 
+    if (saveBND == 0) {
+        std::cout << "Conversion completed successfully." << std::endl;
+    } else {
+        std::cerr << "Conversion failed." << std::endl;
+    }
+
+    // case 2
+    levelSet.clearMaterialProperties();
+    levelSet.setMaterialProperties("Si3N4_LPCVD", 0.3, 0.01);
+    levelSet.setMaterialProperties("Polymer", 1, 0.01);
+    levelSet.setMaterialProperties("SiO2_PECVD", 0.6, 0.01);
+    levelSet.setSTEPS(120);  // maxSteps
+    surfaceFile = "Polymer_etch.obj";
+    outputBNDfile = "Polymer_etch.bnd";
+    surfaceFile = outputFile + surfaceFile;
+    outputBNDfile = outputFile + outputBNDfile;
+
+    std::cout << "Running level set evolution..." << std::endl;
+    if (!levelSet.evolve()) {
+        std::cerr << "Evolution failed. Exiting." << std::endl; return 1;
+    }
+    std::cout << "Saving surface mesh to " << surfaceFile << "..." << std::endl;
+    if (!levelSet.extractSurfaceMeshCGAL(surfaceFile)) {
+        std::cerr << "Failed to save surface mesh. Exiting." << std::endl;
+        return 1;
+    }
+
+    saveBND = ConvertOBJToDFISE(surfaceFile, outputBNDfile); 
+    if (saveBND == 0) {
+        std::cout << "Conversion completed successfully." << std::endl;
+    } else {
+        std::cerr << "Conversion failed." << std::endl;
+    }
+
+    // case 3
+    levelSet.clearMaterialProperties();
+    levelSet.setMaterialProperties("Si3N4_LPCVD", 1, 0.01);
+    levelSet.setMaterialProperties("Polymer", 0.1, 0.01);  
+    levelSet.setMaterialProperties("SiO2_PECVD", 0.4, 0.01); 
+    levelSet.setMaterialProperties("SiO2_Thermal", 0.7, 0.01);  
+    levelSet.setMaterialProperties("Si_Amorph", 0.35, 0.01); 
+    levelSet.setMaterialProperties("Si_Xtal", 0.35, 0.01);
+    levelSet.setSTEPS(160);  // maxSteps
+    surfaceFile = "Nitride_etch.obj";
+    outputBNDfile = "Nitride_etch.bnd"; 
+    surfaceFile = outputFile + surfaceFile;
+    outputBNDfile = outputFile + outputBNDfile;
+
+    std::cout << "Running level set evolution..." << std::endl;
+    if (!levelSet.evolve()) {
+        std::cerr << "Evolution failed. Exiting." << std::endl; return 1;
+    }
+    std::cout << "Saving surface mesh to " << surfaceFile << "..." << std::endl;
+    if (!levelSet.extractSurfaceMeshCGAL(surfaceFile)) {
+        std::cerr << "Failed to save surface mesh. Exiting." << std::endl;
+        return 1;
+    }
+
+    saveBND = ConvertOBJToDFISE(surfaceFile, outputBNDfile); 
+    if (saveBND == 0) {
+        std::cout << "Conversion completed successfully." << std::endl;
+    } else {
+        std::cerr << "Conversion failed." << std::endl;
+    }
+
+    std::cout << "Level set method completed successfully." << std::endl;
+    
+    return 0;
+}
